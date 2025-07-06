9815f866764106ca35e2e3d88886d510cafe8b89
kaaji <rt273565@gmail.com>
2025-07-06 15:14:02 +0545

    font

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
